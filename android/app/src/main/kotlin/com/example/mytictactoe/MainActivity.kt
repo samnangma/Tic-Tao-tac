cc53678fb87ce650da22ebacaa0b1886bb217f83
Samnang MA <masamnang6@gmail.com>
2023-09-14 08:15:29 +0700

    first commit

---
+++ android/app/src/main/kotlin/com/example/mytictactoe/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mytictactoe
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/constants/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class MainColor {
+  static Color primaryColor = Color.fromARGB(255, 200, 51, 51);
+  static Color secondaryColor = Color(0xFFffca27);
+  static Color accentColor = Color(0xFF4169e8);
+}
